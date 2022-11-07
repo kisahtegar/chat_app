b0c6f9e1f5ccf43ac0cc9adbe8272187b80ca825
kisah <data.kisahtegar@gmail.com>
2022-11-07 19:29:10 +0700

    adding Injection container and GetSingleUser cubit

---
+++ lib/features/presentation/cubit/user/get_single_users/get_single_user_state.dart
@@ -0,0 +1,20 @@
+part of 'get_single_user_cubit.dart';
+
+abstract class GetSingleUserState extends Equatable {
+  const GetSingleUserState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetSingleUserInitial extends GetSingleUserState {}
+
+class GetSingleUserLoading extends GetSingleUserState {}
+
+class GetSingleUserLoaded extends GetSingleUserState {
+  final UserEntity userEntity;
+
+  const GetSingleUserLoaded({required this.userEntity});
+}
+
+class GetSingleUserFailure extends GetSingleUserState {}
